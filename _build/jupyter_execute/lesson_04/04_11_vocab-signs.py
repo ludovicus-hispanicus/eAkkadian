6b5b21aa4885de05dda44aea792cacf09bafc5d2
Luis Sáenz <92102767+ludovicus-hispanicus@users.noreply.github.com>
2022-12-09 13:59:43 +0200

    09-12-2022_1

--- _build/jupyter_execute/lesson_04/04_11_vocab-signs.py
+++ _build/jupyter_execute/lesson_04/04_11_vocab-signs.py
@@ -2,3 +2,33 @@
 # coding: utf-8
 
 # # L4.11. Vocabulary & Signs
+# 
+# ## Verbs
+# | Verbs          | Dur., Pret. Root |  Translation |
+# | ------------   | ---------------- | ------------ |
+# | *ahāzum* (a-u) | *ihhaz*, *īhuz*, \**ʾhz* | to seize, hold, take; to take (a wife), marry, to learn |
+# | *akālum* (a-u) | *ikkal*, *īkul*, \**ʾkl* | to do a favour; to be kind to, spare |
+# | *alākum*       | *illak*, *illik*, \**ʾlk* | to go |
+# | *amārum* (a-u) | *immar*, *īmur*, \**ʾmr* | to see |
+# | *apālum* (a-u) | *ippal*, *īpul*, \**ʾpl* | to pay, answer  |
+# | *ebēbum* (i)   | *ibbib*, *ībub*, \**ʾbb* | to be(come) bright, pure |
+# | *edēšum* (i)   | *iddiš*, *īdiš*, \**ʾdš* | to be(come) new |
+# | *epēšum* (a-u,u) | *ippeš*, *īpuš*, \**ʾpš* | to make, do |
+# | *erēbum* (u)   | *irrub*, *īrub*, \**ʾrb* | to cross over |
+# | *ešērum* (i)   | *iššir*, *īšir*, \**ʾšr* | to be/go well; be straight, fair |
+# | *nadānum* (i)  | *iddin*, *iddin*, \**ndn* | to give |
+# | *nakārum* (i)  | *inakkir*, *īkkir*, \**nkr* | to be(come) different; (e)strange(d); hostile |
+# | *naqārum* (u)  | *inaqqar*, *iqqur*, \**nqr* | to demolish; scratch |
+# | *naṣārum* (a-u) | *inaqqar*, *iqqur*, \**nṣr* | einreißen, zerstören |
+# | *naṭālum* (a-u) | *ina︎ṭṭar*, *iṭṭur*, \**nṭr* | to look |
+# | *nawārum* (i)  | *ina︎wwar*, *iṭṭur*, \**nṭr* | to look |
+# | *wabālum*      | *ubbal*, *ūbil*, \**wbl* | to carry, bring |
+# | *walādum*      | *ullad*, *ūlid*, \**wld* | to give birth (to) |
+# | *wašābum*      | *uššab*, *ūšib*, \**wšb* | to sit, live |
+# | *watārum*      | *ittir*, *ītir*, \**wtr* | to be(come) outsize, surplus |
+# 
+# ## Nouns
+# | Nouns          | Plural             | Status Constructus | Translation |
+# | ------------   | ------------------ | ------------------ | ----------- |
+# | *šattum* fem.  | *šanātum*          | *šatti, šattašu*   | year        |
+# | *šuttum* oder *šunatum* fem. | *šunātum* | *šutti, šuttašu* | dream    |
--- _build/jupyter_execute/lesson_04/04_12_assignment.py
+++ _build/jupyter_execute/lesson_04/04_12_assignment.py
@@ -2,3 +2,19 @@
 # coding: utf-8
 
 # # L4.12. Assignment
+# 
+# # Tranliterate and Translate
+# ```{admonition} Example
+# <font size="11">𒄿𒋫𒄩𒊍</font> = *i-ta-ha-az* = he has taken
+# ```
+# 
+# 1. <font size="11">𒄿𒄴𒄩𒊍</font>
+# 2. <font size="11">𒄿𒆪𒌌</font>
+# 3. <font size="11">𒄿𒅋𒆷𒀝</font>
+# 4. <font size="11">𒄿𒋫𒈥</font>
+# 5. <font size="11">𒄿𒁲𒅖/font>
+# 6. <font size="11">𒄿𒁍𒍑</font>
+# 7. <font size="11">𒅅𒆪𒌨</font>
+# 8. <font size="11">𒌒𒁀𒀠</font>
+# 9. <font size="11">𒌑𒇷𒀉</font>
+# 10. <font size="11">𒍑𒁍</font>
