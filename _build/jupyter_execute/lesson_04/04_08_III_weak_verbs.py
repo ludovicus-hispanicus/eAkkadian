50278bf6c6b1fd271ed14432ea6ff0fecf5d82d4
Luis Sáenz <92102767+ludovicus-hispanicus@users.noreply.github.com>
2022-12-13 00:21:46 +0200

    13-12-2022_01

--- _build/jupyter_execute/lesson_04/04_08_III_weak_verbs.py
+++ _build/jupyter_execute/lesson_04/04_08_III_weak_verbs.py
@@ -29,3 +29,94 @@
 # - *imnu* and *imni* "he counted"
 # ```
 # 
+# Verbs III *ī/ē* with following *a* can be contracted or uncontracted:
+# 
+# ```{admonition} Example
+# - *iqbiam* < *iqbi-am* "he said to me".
+# - *ibniā* and *ibnâ* "They (fem.) built".
+# ```
+# 
+# Otherwise contracted:
+# 
+# ```{admonition} Example
+# *išemmû* < \**išemmeū* "they hear"
+# *ikallû* < \**ikallaū* "they hold back"
+# *iqbû* <\**iqbiū* "they said"
+# ```
+# 
+# The *e*-class often changes *a* in neighbouring syllables:
+# 
+# ```{admonition} Example
+# *išemme* < *išamme* "he hears"
+# *teleqqe* < \**talaqqe* "you take"
+# *iqbû* <\**iqbiū* "they said"
+# ```
+# 
+# ``` {warning}
+# With the addition of endings like the personal suffixes, ventive, etc. the original length of the shortened final vowels is restored:
+# - *ibnīšu* 
+# - *ibnīma*
+# - *išmēma*
+# - *imnūma*
+# ```
+# 
+# ## Conjugation of the verbs III-*ī*
+# |         | Durative        |  Perfect        | Preterite    
+# | ------- | --------------  | ---------       | ---------         
+# | 3s.c.   | *ibanni*        | *ibtani*        | *ibni*           
+# | 2s.m.   | *tabanni*       | *tabtani*       | *tabni*          
+# | 2s.f.   | *tabannî*       | *tabtanî*       | *tabnî*         
+# | 1s.c.   | *abanni*        | *abtani*        | *abni*          
+# | 3pl.m.  | *ibannû*        | *ibtanû*        | *ibnû*         
+# | 3pl.f.  | *ibinnâ*        | *ibtanâ*        | *ibnâ*         
+# | 2pl.c.  | *tabannâ*       | *tabtanâ*       | *tabnâ*        
+# | 1pl.c.  | *nibanni*       | *nibtani*       | *nibni*  
+# 
+# ## Conjugation of the verbs III-*ē*
+# |         | Durative        |  Perfect        | Preterite    
+# | ------- | --------------  | ---------       | ---------         
+# | 3s.c.   | *išemme*        | *išteme*        | *išme*           
+# | 2s.m.   | *tešemme*       | *tešteme*       | *tešme*          
+# | 2s.f.   | *tešemmê*/*tešemmî* | *teštemî*/*teštemê* | *tešmî*/*tešmê*         
+# | 1s.c.   | *ešemme*        | *ešteme*        | *ešme*          
+# | 3pl.m.  | *išemmû*        | *ištemû*        | *išmû*         
+# | 3pl.f.  | *išemmeā*/*išemmâ* | *ištemeā*/*ištemâ* | *išmeā*/*išmâ*         
+# | 2pl.c.  | *tešemmeā*/*tešemmâ* | *teštemeā*/*teštemâ* | *tešmeā*/*tešmâ*        
+# | 1pl.c.  | *nišemme*       | *ništeme*       | *nišme*  
+# 
+# ## Conjugation of the verbs III-*ā*
+# |         | Durative        |  Perfect        | Preterite    
+# | ------- | --------------  | ---------       | ---------         
+# | 3s.c.   | *ikalla*        | *iktala*        | *ikla*           
+# | 2s.m.   | *takalla*       | *taktala*       | *takla*          
+# | 2s.f.   | *takallî*       | *taktalî*       | *taklî*         
+# | 1s.c.   | *akalla*        | *aktala*        | *akla*          
+# | 3pl.m.  | *ikallû*        | *iktalû*        | *iklû*         
+# | 3pl.f.  | *ikallâ*        | *iktalâ*        | *iklâ*         
+# | 2pl.c.  | *takallâ*       | *taktalâ*       | *taklâ*        
+# | 1pl.c.  | *nikalla*       | *niktala*       | *nikla*  
+# 
+# ## Conjugation of the verbs III-*ū*
+# |         | Durative        |  Perfect        | Preterite    
+# | ------- | --------------  | ---------       | ---------         
+# | 3s.c.   | *imannu*        | *imtanu*        | *imnu*           
+# | 2s.m.   | *tamannu*       | *tamtanu**      | *tamnu*          
+# | 2s.f.   | *tamannî*       | *tamtanî*       | *tamnî*         
+# | 1s.c.   | *amannu*        | *amtanu*        | *amnu*          
+# | 3pl.m.  | *imannû*        | *imtanû*        | *imnû*         
+# | 3pl.f.  | *imannâ*        | *imtanâ*        | *imnâ*         
+# | 2pl.c.  | *tamannâ*       | *tamtanâ*       | *tamnâ*        
+# | 1pl.c.  | *nimannu*       | *nimtanu*       | *nimnu*
+# 
+# ## Paradigm of the Verbs III-weaks
+# |               | Durative        | Perfect    | Preterite    | Participle    | Infinitve | Verbal Adjective |
+# | -------       | --------------  | --------   | ---------    | ------------  | --------  | ---------------  | 
+# | G III-*ī*     | *ibanni*        | *ibtani*   | *ibni*       | *bānû*        | *banû*    | *banû*           |
+# | G III-*ē*     | *išemme*        | *išteme*   | *išme*       | *šēmû*        | *šemû*    | *šemû*           |
+# | G III-*ā*     | *ikalla*        | *iktala*   | *ikla*       | *kālû*        | *kalû*    | *kalû*           | 
+# | G III-*ū*     | *imannu*        | *imtanu*   | *imnu*       | *manû*        | *manû*    | *kalû*           |
+# 
+# - *banû* "to build"
+# - *šemû* "to hear"
+# - *kalû* "to hold back"
+# - *manû* "to count"
