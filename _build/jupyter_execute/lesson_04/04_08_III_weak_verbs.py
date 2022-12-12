6a496990e78fb50d8c5211bea1380269d4ecfc54
Luis Sáenz <92102767+ludovicus-hispanicus@users.noreply.github.com>
2022-12-12 21:19:36 +0200

    12-12-2022_02

--- _build/jupyter_execute/lesson_04/04_08_III_weak_verbs.py
+++ _build/jupyter_execute/lesson_04/04_08_III_weak_verbs.py
@@ -2,12 +2,30 @@
 # coding: utf-8
 
 # # L4.8: III-Weak Verbs
-# There are two groups:
+# There are two groups of III-weaks verbs:
 # 
 # 1. Roots that consist of two consonants and have a long vowel *ī* or *ū*
 # 
+#     ```{admonition} Example
+#     *banûm* "to build" with *ī*
+#     *manûm* "to count" with *ū*
+#     ```
+# 2. Verbs III-*ʾ that can also be divided into two subgroups:
+#    - Verbs III-*ʾ* of *ʾ*₁: **without** coloring *a* > *e*.
+#    - Verbs III-*ʾ* of *ʾ*₃, *ʾ*₄, *ʾ*₅: **with** coloring *a* > *e*.
+# 
+# In all Verbs III-weak the originally long vowel shortens in final position:
+# 
 # ```{admonition} Example
-# *banûm* "to build" with *ī*
-# *manûm* "to count" with *ū*
+# - With *ī*: *ibni* < \**ibnī* "he built"
+# - With *ū*: *imnu* < \**imnū* "hecounted"
+# - With *ā*: *ikla* < \**iklā* "he restrained"
+# - With *ē*: *ešme* < \**ešmē* "he heard" 
 # ```
-# 2. Verbs III-*ʾ*
+# 
+# The transfer from one group to the other is frequent:
+# 
+# ```{admonition} Example
+# - *imnu* and *imni* "he counted"
+# ```
+# 
