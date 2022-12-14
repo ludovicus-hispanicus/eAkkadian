339ae547124d1c877dc7b7625f1e1987ee1e75b4
Luis Sáenz <92102767+ludovicus-hispanicus@users.noreply.github.com>
2022-12-14 22:59:27 +0200

    14-12-2022_02

--- _build/jupyter_execute/lesson_04/04_00_intro.py
+++ _build/jupyter_execute/lesson_04/04_00_intro.py
@@ -12,7 +12,7 @@
 # - L4.4. [Verbs I-*w*](04_06_1_w_verbs)
 # - L4.5. [Verbs I-*y*](04_07_1_y_verbs)
 # - L4.6. [Verbs III-weak](04_08_III_weak_verbs)
-# - L4.7. [Subjunctive](04_09_subjunctive)
+# - L4.7. [Subjunctive](04_09_subordinator)
 # - L4.8. [Exercises](04_10_exercises)
 # - L4.9. [Vocabulary & Signs](04_11_vocab-signs)
 # - L4.10. [Assignment](04_12_assignment)
