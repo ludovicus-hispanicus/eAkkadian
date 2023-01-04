5618f05580f217464a82c850402032a6971df782
Luis Sáenz <92102767+ludovicus-hispanicus@users.noreply.github.com>
2023-01-04 20:51:43 +0200

    04-01-2022_01

--- _build/jupyter_execute/lesson_07/07_01_d-stem.py
+++ _build/jupyter_execute/lesson_07/07_01_d-stem.py
@@ -2,12 +2,241 @@
 # coding: utf-8
 
 # ```{margin}
-# GoA 33.2
+# GoA 24.1-24.3
 # ```
 # 
 # # L7.1: D-stem
+# The D-stem denotes a factitive intensification of the G-stem. 
+# 
+# ## Funktion
+# 
+# 1. **Factitive** of adjectival verbs:
+#    ```{admonition} Example
+#    - *damāqum* "te be(come) good" → *dummuqum* = "to make good, improve"
+#    - *rapāšum* "to be(come) wide" → *ruppušum*
+#    ```
+# 
+# 2. **Causative** of active-intransitive verbs:
+#    ```{admonition} Example
+#    - *halāqum* "to dissapear" → *hulluqum* "to cause to persih, to destroy"
+#    - *pahārum* "to gather" (intr.) → *puhhurum* "to gather" (trans.)
+#    ```
+# 
+# 3. **Pluralic**, when there is a plurality of objects or subjects:
+#    ```{admonition} Example
+#    *naqāru* "to demolish; scratch" → *nuqqurum* = "demolish (many objects)" 
+#    - G-stem: *ālšu anaqqar* "I will demolish his city"
+#    - D-stem: ālānīšunu unaqqar "I will demolish their cities"
+# 
+#    *našāqu* "to kiss" → *nuššuqu* "to kiss (many times)"
+#    - G-stem: *šēpīja* "he kissed my feet"
+#    - D-stem: *šēpīya unaššiqū* "they kissed my feet" 
+#    ```
+# 4. **Derivative**, i.e. verbs derived from a noun:
+#    ```{admonition} Example
+#    - *ruggubum* "to roof over" < *rugbum* "roof"
+#    - *gullubum* "to shear, to shave" < *gallābum* "barber, hairdresser"
+#    ``` 
+#    ```{note}
+#    These verbs are not attested in G-stem.
+#    ```
+# 
+# 5. **Lexical**, i.e. verbs whose basic form is the D-stem:
+#    ```{admonition} Example
+#    - *kullumum* "to show" 
+#    - *wuššurum* "to release"
+#    ``` 
+#    ```{note}
+#    These verbs are not attested in G-stem as well.
+#    ```  
+# 
+# 6. Not all verbs occur in the D-stem.
+# 7. Sometimes the diffenrece in meaning is not distinguishable 
+#    1. *petûm* (G) ↔ *puttûm* (D) = "to open"
+# 
+# ## Morphological Characteristics
+# 
+# 1. It is characterized by a double middle radical in all its forms and the 
+#    
+#     |               | D-Stem      | 
+#     | -------       | -------------- | 
+#     | Durative      | *upa**rr**as*   | 
+#     | Perfect       | *upta**rr**is*  | 
+#     | Preterite     | *upa**rr**is*    | 
+#     | Imperative    | *pu**rr**is*   |  
+#     | Participle    | *mupa**rr**isum*   |
+#     | Verbal Adj.   | *pu**rr**usum*  | 
+#     | Infinitive    | *pu**rr**usum*   | 
+#     | Stative       | *pu**rr**us*  | 
+# 
+# 2. All verbs have the vowel class *a-i*
+# 
+#     |               | D-Stem      | 
+#     | -------       | -------------- |
+#     | Durative      | *uparr**a**s*  | 
+#     | Preterite     | *uparr**i**s*  | 
+# 
+#     ```{note}
+#     The perfect *uptarris* has the vowel *i* of the preterite rather than that of the Durative as in the G-stem.
+#     - G-stem: *iparras* ↔ *iptaras*
+#     - D-stem: *uparras* ↔ *uptarris*
+#     ```
+# 
+# 3. All personal prefixes have *u-*
+#    
+#     |         | Durative       | Perfect         | Preterite      |
+#     | ------- | -------------- | -------------- | --------------  |  
+#     | 3s.c.   | ***u**parras*  | ***u**ptarris* | ***u**parris*
+#     | 2s.m.   | *t**u**parras* | *t**u**ptarris* | *t**u**parris*
+#     | 2s.f.   | *t**u**parrasī*| *t**u**ptarrisī*| *t**u**parrisī*
+#     | 1s.c.   | ***u**parras*  | ***u**ptarris*  | ***u**parris*
+#     | 3pl.m.  | ***u**parrasū* | ***u**ptarrisū*| ***u**parrisū*
+#     | 3pl.f.  | ***u**parrasā* | ***u**ptarrisā*| ***u**parrisā*
+#     | 2pl.c.  | *t**u**parrasā* | *t**u**ptarrisā*| *t**u**parrisā*
+#     | 1pl.c.  | *n**u**parras* | *n**u**ptarris*| *n**u**parris*
+# 
+#     ```{note}
+#     The *-t-* of the perfect undergoes the assimilation before g, d, ṭ, s, ṣ and z as in the G-stem: 
+#     - *uṣṣabbit*
+#     - *ussahher*
+#     ```
+# 
+# 4. The imperative has the *i* fronm the preterite:
+# 
+#     | 2ms       | 2fs      | pl.com. |
+#     | --------  | -------- | ------- |
+#     | *purr**i**s*  | *purr**i**sī*  | *purr**i**sā* |
+# 
+# 5. The participle has the prefix *mu-*
+# 
+#     |               | D-Stem      | 
+#     | -------       | -------------- |
+#     | Participle    | *mupa**rr**isum* |
+# 
+# ```{note}
+# The particples of all stems other than the G-stem have the prefix *mu-*
+# ```
+# 6. The **infinitive** and the **verbal adjective** are identical: 
+# 
+#     |               | D-Stem      | 
+#     | -------       | -------------- | 
+#     | Infinitive    | *pu**rr**usum* | 
+#     | Verbal Adj.   | *pu**rr**usum* |
+# 
+# 7. In verbs with the e-coloring, *a* vowels can become *e* but not always
+# 
+#    - D-stem Durative: *uqerreb* or *uqarrab*
+#    - D-stem preterite: *uqerrib* or *uqarrib*
+# 
+#    ```{warning}
+#    The orthography for *uqerreb* (durative) and *uqerrib* (preterite) is identical:
+#    - <font size="11">𒌑𒆠𒅕𒊑𒅁</font> = *u₂-qe₂-re-eb* = *uqerreb*
+#    - <font size="11">𒌑𒆠𒅕𒊑𒅁</font> = *u₂-qe₂-ri-ib* = *uqerrib*
+#    ```
+# 
+# ## I-*n* Verbs
+# The assimilation of the *n* only takes place in the perfect
+# |               | I-*n*      | 
+# | -------       | -------------- | 
+# | Durative      | *unakkar*   | 
+# | Perfect       | *u**tt**aker*  | 
+# | Preterite     | *unakker*    | 
+# | Imperative    | *nukker*   |  
+# | Participle    | *munakkerum*   |
+# | Infinitive    | *nukkurum*   |
+# | Verbal Adj.   | *nukkurum*  |
+# | Stative       | *nukkur*   |
 # 
 # 
+# ## I-*ʾ* Verbs
+# For these verbs we find the same rules as in the G-stem
+# 
+# |               | *arākum*   | *epēšum*  | *elûm*    |
+# | -------       | ---------- | --------- | --------- |
+# | Durative      | *urrak*    | *uppaš*   | *ulla*
+# | Perfect       | *ūtarrik*  | *ūteppiš* | *ūtelli*
+# | Preterite     | *urrik*    | *uppiš*   | *ulli*
+# | Imperative    | *urrik*    | *uppiš*   | *ulli*
+# | Participle    | *murrikum* | *muppišum* | *mullûm*
+# | Infinitive    | *urrukum*  | *uppušum* | *ullûm*
+# | Verbal Adj.   | *urrukum*  | *uppušum* | *ullûm*
+# | Stative       | *urruk*    | *uppuš*   | *ullu*  
 # 
 # 
+# ## I-*w*
+# For these verbs we find the same rules as in the G-stem. The adjectival as well as the active verbs are conjugated alike. 
 # 
+# |               | *watārum*  | 
+# | -------       | ---------- | 
+# | Durative      | *uwattar*    | 
+# | Perfect       | *ūtatter*  | 
+# | Preterite     | *uwatter*    | 
+# | Imperative    | *(w)utter*    | 
+# | Participle    | *muwatterum* | 
+# | Infinitive    | *(w)utturum*  |
+# | Verbal Adj.   | *(w)utturum*  |
+# | Stative       | *(w)uttur*    |
+# 
+# 
+# ```{note}
+# - In the perfect: *uw-* > *ū*
+# - *w* at the beginnig is often dropped: *(w)utturum*, *(w)utter*
+# ```
+# 
+# ## II-weak
+# These verbs have to be separated into two groups
+# 
+# ### II-*w* and II-*y*
+# They follow the same rules as the G-stem. As for the G-stem, when a vowel follows the *R₃*, this is doubled, that is also true for the infinitve: 
+# 
+# |               | *kânum*  | 
+# | -------       | ---------- | 
+# | Durative      | *ukān*, *ukannū* | 
+# | Perfect       | *uktīn*, *ukannū* | 
+# | Preterite     | *ukīn*, *ukinnū* | 
+# | Imperative    | *kīn*, *kinnā* | 
+# | Participle    | *mukinnum* |
+# | Infinitive    | *kunnum* |  
+# | Verbal Adj.   | *kunnum* |
+# | Stative       | *kūn*, *kunnu* | 
+# 
+# ### II-*ʾ*
+# Verbs II-*ʾ* in D-stem are poorly attested. The verbs II₁-₂ (II-*a*) tend to be conjugated with the middle as a strong consonant, although contraction may take place.
+# 
+# |               | *wârum* strong        | *wârum* weak | 
+# | -------       | --------------        | ------------ |
+# | Durative      | *uwaʾʾar*, *uwaʾʾarū* | *uwâr*, *uwârrū* | 
+# | Perfect       | *ūtaʾʾer*,            |                  | 
+# | Preterite     | *uwaʾʾer*, *uwaʾʾerū* | *uwêr*, *uwêrrū* |
+# | Imperative    | *wuʾʾer*              |                  | 
+# | Participle    | *muwaʾʾerum*          |
+# | Infinitive    | *wuʾʾurum*            | *wûrum* 
+# | Verbal Adj.   | *wuʾʾurum*            | *wûrum* 
+# | Stative       | *wuʾʾur*              | 
+# 
+# The verbs II₃-₅ (II-*e*) are treated like II-w/y but with *e*-vowels or conjugated like II₁-₂ (II-*a*):
+# 
+# |               | *rêqum*  | 
+# | -------       | ---------- | 
+# | Durative      | *urēq*, *ureqqū* | 
+# | Perfect       | *urtīq*, *urtiqqū* | 
+# | Preterite     | *urīq*, *uriqqu* | 
+# | Imperative    | *rīq*, *riqqā* | 
+# | Participle    | *muriqqum* |
+# | Infinitive    | *ruqqum* |  
+# | Verbal Adj.   | *ruqqum* |
+# | Stative       | *rūq*, *ruqqu* | 
+# 
+# ## III-Weak
+# Most of these verbs fall together into one paradigm, only the verbs III-*e* keep the *e*, when *a* is expected:
+# 
+# |               | *banûm*   | *ṭehûm*   | 
+# | -------       | --------- | --------  |
+# | Durative      | *ubanna*  | *uṭehhe*/*uṭahha*   | 
+# | Perfect       | *ubtanni* | *uṭṭehhi*/*uṭṭahhi* | 
+# | Preterite     | *ubanni*  | *uṭehhi*/*uṭahhi*   |
+# | Imperative    | *bunni*   | *ṭuhhi*   | 
+# | Participle    | *mubannûm* | *muṭehhûm*/*muṭahhûm* |
+# | Infinitive    | *bunnûm*  | *ṭuhhûmm* 
+# | Verbal Adj.   | *bunnûm*  | *ṭuhhûmm* 
+# | Stative       | *bunnu*   | *ṭuhhu*
