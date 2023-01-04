ab0e3405ecf5799856160301d3696c9c368ddb9e
Luis Sáenz <92102767+ludovicus-hispanicus@users.noreply.github.com>
2023-01-04 21:57:29 +0200

    04-01-2023_02

--- _build/jupyter_execute/lesson_07/07_04_exercises.py
+++ _build/jupyter_execute/lesson_07/07_04_exercises.py
@@ -3,3 +3,45 @@
 
 # # L7.4: Exercises
 # 
+# ## D-Stem
+# Analyse and translate the following forms:
+# 
+# 1. *ulammid*
+# 2. *uballitūka*
+# 3. *ugallabūki*
+# 4. *tuštebir*
+# 5. *šipram udammiqū*
+# 
+# Translate
+# 1. *<sup>md</sup>Anum-muballiṭ*
+# 2. *bēlum muballiṭ Uruk šākin mê ana nišīšu*
+# 3. *anāku šarrum lā mušallim mātīšu*
+# 
+# Normalize and translate CH § 1
+# *šum-ma a-wi-lum a-wi-lam ú-ub-bi-ir-ma ne-er-tam e-li-šu id-di-ma la uk-ti-in-šu mu-ub-bi-ir-šu id-da-ak*
+# 
+# ## Š-Stem
+# Analyse and translate the following forms:
+# 
+# 1. *ušašṭar*
+# 2. *tušakniš*
+# 3. *mušakniš nakrim*
+# 4. *mušalbir bālaṭ šarram*
+# 5. *ina libbi ālim tušaškanšunūti*
+# 
+# Translate
+# 1. *ilū šarratam ālša ušakšidū*
+# 2. *ana šupluh mātim ṣabam upahhir*
+# 3. *Nintu šulput mātīšu ina pī Enlil ušaškin*
+# 4. *Adad mušaškin hegallim*
+# 
+# Normalize and translate CH § 127
+# *šum-ma a-wi-lum e-li NIN.DINGIR u₃ aš-ša-at a-wi-lim u₂-ba-nam u₂-ša-at-ri-iṣ-ma la uk-ti-in a-wi-lam šu-a-ti ma-har da-a-a-ni i-na-aṭ-ṭu₃-u₂-šu u₃ mu-ut-ta-su₂ u₂-gal-la-bu*
+# 
+# C. Transliterate and translate
+# Beginning of CH
+# 1. <font size="11">𒉌𒉡𒀭𒍢𒊒𒌝</font>
+# 2. <font size="11">𒈗𒀭𒀀𒉣𒈾𒆠</font>
+# 3. <font size="11">𒀭𒂗𒆤</font>
+# 4. <font size="11">𒁁𒂖𒊭𒈨𒂊</font>
+# 5. <font size="11">𒅇𒅕𒍢𒁴</font>
